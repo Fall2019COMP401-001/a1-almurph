2bd1dde781c523db78698f6a57523b1ea9b5d174
aswagert <aswagert@172.20.4.12>
2019-08-31 16:02:47 -0400

    got rid of the classes and restarted

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -19,7 +19,74 @@ public class A1Adept {
 			storePrices[i] = scan.nextDouble(); // for loop that cycles through the items in the store
 		}
 		
+		int numCustomers = scan.nextInt(); // counts the number of customers
+		// String[] firstName = new String[numCustomers]; 
+		// String[] lastName = new String[numCustomers]; 
+		
+		String[] customers = new String[numCustomers]; 
+		Double[] amountSpent = new Double[numCustomers]; 
+		
+		// double cost = 0.0; 
+		
+		for (int i = 0; i < numCustomers; i++) {
+			// firstName[i] = scan.next(); 
+			// lastName[i] = scan.next(); 
+			customers[i] = scan.next() + " " + scan.next(); // sets up the customers name
+			
+			int numBought = scan.nextInt(); 
+			double cost = 0.0; 
+			
+			for (int j = 0; j < numBought; i++) {
+				int quantityOfItems = scan.nextInt(); 
+				String itemDescription = scan.next(); 
+				
+				for (int k = 0; k < numItems; k++) {
+					if (itemDescription.equals(storeItems[k])) {
+						cost += storePrices[k] * quantityOfItems; 
+					}
+				}
+			
+			
+		
+		
+		// Customers[] customer = new Customers[numCustomers];
+		
+//		for (int i=0; i<numCustomers; i++) {
+//			Customers people = new Customers();
+//			customer[i] = people;
+//		}
+//		
+//		
+//		for (int i = 0; i < numCustomers; i++) {
+//			String firstName = scan.next();
+//			String lastName = scan.next();
+//			
+//			// itemsBought[i] = scan.nextInt(); 
+//			int itemCheck = scan.nextInt();
+//			customer[i].firstName = firstName;
+//			customer[i].lastName = lastName;
+//			customer[i].itemsBought = itemCheck;
+//			
+//			customer[i].itemDescription = new String[itemCheck];
+//			customer[i].quanitityOfItems = new int[itemCheck];
+//			for(int j=0; j<itemCheck; j++) {
+//				int quantityOfItems = scan.nextInt();
+//				String itemDescription = scan.next(); 
+//				customer[i].quanitityOfItems[j] = quantityOfItems; 
+//				customer[i].itemDescription[j] = itemDescription; 
+//				
+			}
+			
+			amountSpent[i] = cost; 
+				
+			}
+			
+		}
+		
+		
+		
+		
+		
 		
 		
 	}
-}
