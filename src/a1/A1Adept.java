34d448446795f15ecf50e8ae746cf22cf8aba661
aswagert <aswagert@172.20.4.12>
2019-08-30 21:16:42 -0400

    created loop that cycles through items

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -9,6 +9,17 @@ public class A1Adept {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int numItems = scan.nextInt();  // counts number of items in store
+		
+		String[] storeItems = new String[numItems]; // array of the names of items in the store
+		Double[] storePrices = new Double[numItems]; // array of the price of each item in the store
+	
+		for (int i = 0; i<numItems; i++) {
+			storeItems[i] = scan.next(); 
+			storePrices[i] = scan.nextDouble(); // for loop that cycles through the items in the store
+		}
+		
+		
 		
 	}
 }
