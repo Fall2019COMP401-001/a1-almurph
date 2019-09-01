3cd7db826030357428e5315122d957551ff168ee
aswagert <aswagert@172.20.4.12>
2019-08-31 22:09:10 -0400

    added space

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -95,11 +95,11 @@ public class A1Adept {
 			
 			}
 		
-		System.out.println("Biggest: " + firstName[spendsMost] + " " + lastName[spendsMost] + " " + "(" + String.format("%.02f", amountSpent[spendsMost]) + ")"); 
+		System.out.println("Biggest: " + firstName[spendsMost] + " " + lastName[spendsMost] + " " + "(" + String.format("%.02f", amountSpent[spendsMost]) + ") "); 
 			
-		System.out.println("Smallest: " + firstName[spendsLeast] + " " + lastName[spendsLeast] + " " + "(" + String.format("%.02f", amountSpent[spendsLeast]) + ")"); 
+		System.out.println("Smallest: " + firstName[spendsLeast] + " " + lastName[spendsLeast] + " " + "(" + String.format("%.02f", amountSpent[spendsLeast]) + ") "); 
 		
-		System.out.println("Averaget: " + String.format("%.02f", sumForAverage/numCustomers)); 
+		System.out.println("Averaget: " + String.format("%.02f ", sumForAverage/numCustomers)); 
 		
 		scan.close(); 
 		
