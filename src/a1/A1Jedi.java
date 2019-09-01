17d856b40a59ffcb537ab7a2b7c59ff9292cf4b2
aswagert <aswagert@172.20.4.12>
2019-09-01 17:29:03 -0400

    set up the same as adept

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -10,5 +10,47 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
-	}
+		int numItems = scan.nextInt();  // counts number of items in store
+		
+		String[] storeItems = new String[numItems]; // array of the names of items in the store
+		Double[] storePrices = new Double[numItems]; // array of the price of each item in the store
+	
+		for (int i = 0; i<numItems; i++) {
+			storeItems[i] = scan.next(); 
+			storePrices[i] = scan.nextDouble();	
+		
+		}
+		
+		int numCustomers = scan.nextInt(); // counts the number of customers
+		String[] firstName = new String[numCustomers]; // sets up customer first name
+		String[] lastName = new String[numCustomers]; // sets up customer last name
+		
+		int[] itemQuantity = new int[numItems]; // sets up the total quantity of item bought
+		int[] peopleQuantity = new int[numItems]; // sets up number of customers that bought that item
+		
+		
+		for (int i = 0; i < numCustomers; i++) {
+			firstName[i] = scan.next(); 
+			lastName[i] = scan.next(); //creates first and last name
+
+			int numBought = scan.nextInt();
+			
+			boolean[] wasItBought = new boolean[numItems];
+			for (int j = 0; j < numBought; j++) {
+				int quantity = scan.nextInt(); 
+				String nameOfItem = scan.next(); 
+				
+				for (int k = 0; k < numItems; k++) {
+					if (storeItems[k].equals(nameOfItem)) {
+						itemQuantity[k] += quantity; 
+						
+						if (!wasItBought[k]) {
+							wasItBought[k] = true; 
+							peopleQuantity[k]++; 
+						}
+					}
+				}
+			}
+			
+		}
 }
